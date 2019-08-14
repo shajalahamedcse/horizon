f99abb54a3a3ee06c02d91b3f7e61a7129451402
shajalAhamed <shajalahamedcse@gmail.com>
2019-08-15 00:16:39 +0600

    Added key pair creation method

--- src/pages/key/KeyPair.js
+++ src/pages/key/KeyPair.js
@@ -6,12 +6,46 @@ class KeyPair extends Component{
     constructor(props) {
         super(props);
 
-        this.state = { open: false };
+        this.state = {
+            open: false,
+            name:'' // Key-Par name
+        };
+
+        this.createKeyPair= this.createKeyPair.bind(this);
 
         this.show = size => () => this.setState({ size, open: true });
-        this.close = () => this.setState({ open: false })
+
+        this.close = () => this.setState({ open: false });
+
+        this.handleChange = (event, {name,value}) =>{
+            console.log(value);
+            this.setState({
+                name : value
+            })
+        };
+
+
     }
 
+
+    createKeyPair(){
+        const scopedToken = localStorage.getItem("scopedToken");
+        const header = {
+            headers: {
+                "X-Auth-Token": scopedToken
+            }
+        };
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "http://103.248.13.91:8774/v2.1/os-keypairs";
+        const data = {
+            "keypair":{
+                "name":this.state.name
+            }
+        };
+        console.log(data);
+        const response =  axios.post(proxyurl + url, data,header);
+        console.log(response);
+    }
 
    async componentDidMount() {
        this.setState({ loading: true });
@@ -39,11 +73,11 @@ class KeyPair extends Component{
                 <Modal size={size} open={open} onClose={this.close}>
                     <Modal.Header>Create Key Pair</Modal.Header>
                     <Modal.Content>
-                        <Input focus size='large' placeholder='Enter Key Pair Name' />
+                        <Input focus size='large' placeholder='Enter Key Pair Name' onChange={this.handleChange}/>
                     </Modal.Content>
                     <Modal.Actions>
                         <Button negative onClick={this.close}>Cancel</Button>
-                        <Button positive icon='checkmark' labelPosition='right' content='Create' />
+                        <Button positive icon='checkmark' labelPosition='right' content='Create' onClick={this.createKeyPair}/>
                     </Modal.Actions>
                 </Modal>
             </div>)
