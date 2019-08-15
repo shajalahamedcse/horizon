093e7f2e1880e8eb079ad5cdedfe755701b04dff
shajalAhamed <shajalahamedcse@gmail.com>
2019-08-16 01:10:18 +0600

    Added key pair with UI

--- src/pages/console/compute/dropdowns/Networks.js
+++ src/pages/console/compute/dropdowns/Networks.js
@@ -6,6 +6,7 @@ class Networks extends Component {
   networks = [];
   flavors = [];
   images = [];
+  keypairs = [];
 
   state = {
     isFetching: false,
@@ -14,15 +15,19 @@ class Networks extends Component {
     netValue: "",
     imgValue: "",
     flavorValue: "",
+    keyPairValue: "",
     netOptions: [],
     imgOptions: [],
-    flavorOptions: []
+    flavorOptions: [],
+    keyPairOptions: [],
+
   };
 
   flavorChange = (e, { value }) => this.setState({ flavorValue: value });
   imgChange = (e, { value }) => this.setState({ imgValue: value });
   netChange = (e, { value }) => this.setState({ netValue: value });
   nameChange = (e, { value }) => this.setState({ instanceName: value });
+  keyPairChange = (e, { value }) => { console.log(value); this.setState({keyPairValue: value})};
 
   formSubmit = () => {
     this.setState({ formLoading: true });
@@ -33,9 +38,11 @@ class Networks extends Component {
             "imageRef": this.state.imgValue,
             "networks": [{
                 "uuid": this.state.netValue
-            }]
+            }],
+            "key_name" : this.state.keyPairValue
         }
-    }
+    };
+    console.log(postData);
     const scopedToken = localStorage.getItem("scopedToken");
     const header = {
       headers: {
@@ -54,7 +61,7 @@ class Networks extends Component {
 
     console.log(postData);
 
-}
+};
 
   async componentDidMount() {
     this.setState({ isFetching: true });
@@ -68,15 +75,18 @@ class Networks extends Component {
     const urlNetworks = `http://103.248.13.91:9696/v2.0/networks?shared=True`;
     const urlFlavors = "http://103.248.13.91:8774/v2.1/flavors";
     const urlImages = "http://103.248.13.91:9292/v2.0/images";
+    const urlKeyPairs = "http://103.248.13.91:8774/v2.1/os-keypairs";
 
     axios.all([
         axios.get(proxyurl + urlNetworks, header),
         axios.get(proxyurl + urlFlavors, header),
-        axios.get(proxyurl + urlImages, header)
-    ]).then(axios.spread((networksRes, flavorsRes, imagesRes) => {
+        axios.get(proxyurl + urlImages, header),
+        axios.get(proxyurl + urlKeyPairs,header)
+    ]).then(axios.spread((networksRes, flavorsRes, imagesRes, keyPairRes) => {
         const networksData = networksRes.data.networks;
         const flavorsData = flavorsRes.data.flavors;
         const imagesData = imagesRes.data.images;
+        const keyPairsData = keyPairRes.data.keypairs;
         
         networksData.forEach(net => {
             this.networks.push({ key: net.name, text: net.name, value: net.id });
@@ -90,18 +100,27 @@ class Networks extends Component {
             this.images.push({ key: image.name, text: image.name, value: image.id });
         });
 
+        keyPairsData.forEach(data =>{
+            let keypair = data.keypair;
+            this.keypairs.push({ key: keypair.name, text: keypair.name, value: keypair.name});
+        });
+        console.log(keyPairsData);
+
+
+
         this.setState({
             isFetching: false,
             netOptions: this.networks,
             imgOptions: this.images,
-            flavorOptions: this.flavors
+            flavorOptions: this.flavors,
+            keyPairOptions: this.keypairs,
         });
     })).catch((err)=> {
         console.log(err);
     })
   }
   render() {
-    const { isFetching, imgValue, netOptions, flavorValue, netValue, imgOptions, flavorOptions } = this.state;
+    const { isFetching, imgValue, netOptions, flavorValue, netValue, imgOptions, flavorOptions, keyPairValue,keyPairOptions } = this.state;
     return (
       <div>
 
@@ -110,6 +129,7 @@ class Networks extends Component {
             <Form.Field control={Select} selection onChange={this.flavorChange} loading={isFetching} disabled={isFetching} value={flavorValue} label='Select Flavour' options={flavorOptions} placeholder='Select a Flavour' />
             <Form.Field control={Select} selection onChange={this.imgChange} loading={isFetching} disabled={isFetching} value={imgValue} label='Select Image' options={imgOptions} placeholder='Select an Image' />
             <Form.Field control={Select} selection onChange={this.netChange} loading={isFetching} disabled={isFetching} value={netValue} label='Select Network' options={netOptions} placeholder='Select a Network' />
+            <Form.Field control={Select} selection onChange={this.keyPairChange} loading={isFetching} disabled={isFetching} value={keyPairValue} label='Select Key Pair' options={keyPairOptions} placeholder='Select a Network'/>
             <Form.Field control={Button}>Launch Instance</Form.Field>
       </Form>
       </div>
